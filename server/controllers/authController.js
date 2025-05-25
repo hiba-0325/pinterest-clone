860dc2eacecd4845ec52f1a2894b015b0fc67834
hiba-0325 <hibakm72@gmail.com>
2025-05-25 09:40:09 +0530

    Implement user authentication and authorization middleware

---
+++ server/controllers/authController.js
@@ -0,0 +1,152 @@
+
+import User from '../models/userModel.js';
+
+export const register = async (req, res, next) => {
+  try {
+    const { username, email, password } = req.body;
+
+    const userExists = await User.findOne({
+      $or: [{ email }, { username }]
+    });
+
+    if (userExists) {
+      return res.status(400).json({
+        success: false,
+        message: userExists.email === email
+          ? 'Email already in use'
+          : 'Username already taken'
+      });
+    }
+
+    const user = await User.create({
+      username,
+      email,
+      password
+    });
+
+    const token = user.generateAuthToken();
+    user.password = undefined;
+
+    res.status(201).json({
+      success: true,
+      user,
+      token
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+// Login user
+export const login = async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+      return res.status(400).json({
+        success: false,
+        message: 'Please provide email and password'
+      });
+    }
+
+    const user = await User.findOne({ email }).select('+password');
+
+    if (!user) {
+      return res.status(401).json({
+        success: false,
+        message: 'Invalid credentials'
+      });
+    }
+
+    const isMatch = await user.comparePassword(password);
+
+    if (!isMatch) {
+      return res.status(401).json({
+        success: false,
+        message: 'Invalid credentials'
+      });
+    }
+
+    const token = user.generateAuthToken();
+    user.password = undefined;
+
+    res.status(200).json({
+      success: true,
+      user,
+      token
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const getCurrentUser = async (req, res, next) => {
+  try {
+    const user = await User.findById(req.user._id);
+
+    res.status(200).json({
+      success: true,
+      user
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+// Update user profile
+export const updateProfile = async (req, res, next) => {
+  try {
+    const { username, bio, website } = req.body;
+
+    const user = await User.findByIdAndUpdate(
+      req.user._id,
+      {
+        username,
+        bio,
+        website
+      },
+      {
+        new: true,
+        runValidators: true
+      }
+    );
+
+    res.status(200).json({
+      success: true,
+      user
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+// Update password
+export const updatePassword = async (req, res, next) => {
+  try {
+    const { currentPassword, newPassword } = req.body;
+
+    const user = await User.findById(req.user._id).select('+password');
+
+    const isMatch = await user.comparePassword(currentPassword);
+
+    if (!isMatch) {
+      return res.status(401).json({
+        success: false,
+        message: 'Current password is incorrect'
+      });
+    }
+
+    user.password = newPassword;
+    await user.save();
+
+    const token = user.generateAuthToken();
+
+    res.status(200).json({
+      success: true,
+      message: 'Password updated successfully',
+      token
+    });
+  } catch (error) {
+    next(error);
+  }
+};
